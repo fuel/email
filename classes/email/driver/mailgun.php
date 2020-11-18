55abbdff8cb49276ff9aede3e26e26e7a6f56cde
Matthew Sinclair-Day <development@mingfu.tw>
2020-11-18 09:09:25 +0800

    Conformance to Fuel coding standards

--- classes/email/driver/mailgun.php
+++ classes/email/driver/mailgun.php
@@ -24,7 +24,8 @@ class Email_Driver_Mailgun extends \Email_Driver
 
 		$endpoint = isset($config_mailgun['endpoint']) ? $config_mailgun['endpoint'] : null;
 
-		if (empty($endpoint)) {
+		if (empty($endpoint))
+		{
 			$mg = \Mailgun\Mailgun::create($config_mailgun['key']);
 		}
 		else
@@ -36,7 +37,8 @@ class Email_Driver_Mailgun extends \Email_Driver
 		$exclude = array('From'=>'From', 'To'=>'To', 'Cc'=>'Cc', 'Bcc'=>'Bcc', 'Subject'=>'Subject', 'Content-Type'=>'Content-Type', 'Content-Transfer-Encoding' => 'Content-Transfer-Encoding');
 		$headers = array_diff_key($this->headers, $exclude);
 
-		foreach ($this->extra_headers as $header => $value) {
+		foreach ($this->extra_headers as $header => $value)
+		{
 			$headers[$header] = $value;
 		}
 
@@ -56,15 +58,18 @@ class Email_Driver_Mailgun extends \Email_Driver
 		$this->alt_body and $post_data['text'] = $this->alt_body;
 
 		// Mailgun's "arbitrary headers" are h: prefixed
-		foreach ($headers as $name => $value) {
+		foreach ($headers as $name => $value)
+		{
 			$post_data["h:{$name}"] = $value;
 		}
 
-		foreach ($this->attachments['attachment'] as $cid => $file) {
+		foreach ($this->attachments['attachment'] as $cid => $file)
+		{
 			$post_data['attachment'][] = array('filePath' => $file['file'][0], 'remoteName' => $file['file'][1]);
 		}
 
-		foreach ($this->attachments['inline'] as $cid => $file) {
+		foreach ($this->attachments['inline'] as $cid => $file)
+		{
 			$post_data['inline'][] = array('filePath' => $file['file'][0], 'remoteName' => substr($cid, 4));
 		}
 
